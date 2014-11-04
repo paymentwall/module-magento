84415853982779cfb559fbedaff75d6bdd789840
hieutran-paymentwall <hieu@paymentwall.com>
2014-11-04 11:12:19 +0700

    Magento PW Pro and PW Local

--- src/app/code/community/Paymentwall/Paymentwall/Block/Checkout/Info/Method/Pwlocal.php
+++ src/app/code/community/Paymentwall/Paymentwall/Block/Checkout/Info/Method/Pwlocal.php
@@ -6,7 +6,7 @@
  * 
  * Class Paymentwall_Paymentwall_Block_Checkout_Info_Method_Local
  */
-class Paymentwall_Paymentwall_Block_Checkout_Info_Method_Local extends Mage_Checkout_Block_Onepage_Payment_Info
+class Paymentwall_Paymentwall_Block_Checkout_Info_Method_Pwlocal extends Mage_Checkout_Block_Onepage_Payment_Info
 {
 
 }
