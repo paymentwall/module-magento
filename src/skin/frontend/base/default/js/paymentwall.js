feb721335c0c5f85248620306b11274c69dea20e
dzung2t <tiendung@paymentwall.com>
2015-11-30 11:48:27 +0700

    Fix double token requests

--- src/skin/frontend/base/default/js/paymentwall.js
+++ src/skin/frontend/base/default/js/paymentwall.js
@@ -1,4 +1,5 @@
 var PW = {
+    "rewritePayment": false,
     "initPwBrick": function (publicKey) {
         return new Brick({
             public_key: publicKey,
@@ -56,10 +57,18 @@ var PW = {
     },
     "rewriteSavePayment": function (brick) {
 
+        // Check second load
+        if (this.rewritePayment) {
+            return false;
+        }
+
+        this.rewritePayment = true;
         Payment.prototype.save = Payment.prototype.save.wrap(function (origin_save) {
             if ($('p_method_paymentwall_pwbrick').checked) {
+
                 // Check payment method selected
                 var validator = new Validation(this.form);
+
                 if (this.validate() && validator.validate()) {
                     brick.tokenizeCard(
                         PW.prepareBrickData(),
